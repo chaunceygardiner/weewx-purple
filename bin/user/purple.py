66800b6a9f3ecf0c0d6e09c1295f3a50d52ed93f
John Kline <john@johnkline.com>
2022-10-07 14:02:17 -0700

    Release 3.2.

--- bin/user/purple.py
+++ bin/user/purple.py
@@ -47,7 +47,7 @@ from weewx.engine import StdService
 
 log = logging.getLogger(__name__)
 
-WEEWX_PURPLE_VERSION = "3.1"
+WEEWX_PURPLE_VERSION = "3.2"
 
 if sys.version_info[0] < 3 or (sys.version_info[0] == 3 and sys.version_info[1] < 7):
     raise weewx.UnsupportedFeature(
