1627b6ca31c8805890f0c4951d8bb92084c029ff
Tom Keffer <tkeffer@gmail.com>
2026-08-23 10:29:19 -0700

    Use a ConfigObj formatted configuration stanza.
    Also import ExtensionInstaller from weecfg.extension

--- install.py
+++ install.py
@@ -15,10 +15,71 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 import sys
+from io import StringIO
+
+import configobj
+
 import weewx
+from weecfg.extension import ExtensionInstaller
 
+CONFIG="""
+[StdReport]
+    [[PurpleReport]]
+        # The "PurpleReport" uses the "purple" skin, which showcases the extension.
+        # Images and files are placed in a dedicated subdirectory.
+        HTML_ROOT = public_html/purple
+        enable = false
+        skin = purple
 
-from setup import ExtensionInstaller
+[Purple]
+    # This section is for configuring the extension weewx-purple.
+    # See the README.md for details.
+
+    # How often to poll the sensor in seconds
+    poll_secs = 15
+
+    # Proxies are instances of purple-proxy
+    [[Proxy1]]
+        enable = False
+        hostname = proxy1
+        port = 8000
+        timeout = 5
+    [[Proxy2]]
+        enable = False
+        hostname = proxy2
+        port = 8000
+        timeout = 5
+    [[Proxy3]]
+        enable = False
+        hostname = proxy3
+        port = 8000
+        timeout = 5
+    [[Proxy4]]
+        enable = False
+        hostname = proxy4
+        port = 8000
+        timeout = 5
+
+    # Sensors are hardware instances
+    [[Sensor1]]
+        enable = True
+        # Replace with the host name or IP address of the first sensor
+        hostname = purple-air
+        # Port is usually 80
+        port = 80
+        # http timeout (seconds)
+        timeout = 15
+    [[Sensor2]]
+        enable = False
+        # Replace with the host name or IP address of the second sensor
+        hostname = purple-air2
+        # Port is usually 80
+        port = 80
+        # http timeout (seconds)
+        timeout = 15
+"""
+
+purple_dict = configobj.ConfigObj(StringIO(CONFIG))
 
 def loader():
     if sys.version_info[0] < 3 or (sys.version_info[0] == 3 and sys.version_info[1] < 7):
@@ -38,54 +99,7 @@ class PurpleInstaller(ExtensionInstaller):
             author="John A Kline",
             author_email="john@johnkline.com",
             data_services='user.purple.Purple',
-            config = {
-                'StdReport': {
-                    'PurpleReport': {
-                        'HTML_ROOT':'purple',
-                        'enable': 'true',
-                        'skin':'purple',
-                    },
-                },
-                'Purple': {
-                    'poll_secs'          : 15,
-                    'Proxy1'   : {
-                        'enable'         : False,
-                        'hostname'       : 'proxy1',
-                        'port'           : '8000',
-                        'timeout'        : '5',
-                    },
-                    'Proxy2' : {
-                        'enable'         : False,
-                        'hostname'       : 'proxy2',
-                        'port'           : '8000',
-                        'timeout'        : '5',
-                    },
-                    'Proxy3'  : {
-                        'enable'         : False,
-                        'hostname'       : 'proxy3',
-                        'port'           : '8000',
-                        'timeout'        : '5',
-                    },
-                    'Proxy4': {
-                        'enable'         : False,
-                        'hostname'       : 'proxy4',
-                        'port'           : '8000',
-                        'timeout'        : '5',
-                    },
-                    'Sensor1'  : {
-                        'enable'     : True,
-                        'hostname'   : 'purple-air',
-                        'port'       : '80',
-                        'timeout'    : '15',
-                    },
-                    'Sensor2': {
-                        'enable'     : False,
-                        'hostname'   : 'purple-air2',
-                        'port'       : '80',
-                        'timeout'    : '15',
-                    },
-                },
-            },
+            config = purple_dict,
             files=[
                 ('bin/user', ['bin/user/purple.py']),
                 ('skins/purple', [
